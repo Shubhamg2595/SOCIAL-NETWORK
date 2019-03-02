8a039d18c5f57384caffd60cb59c71468cc1c0a4
Shubham Gupta <shubhamg2595@gmail.com>
2019-03-02 14:22:55 +0530

    User Model Created

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+//@route  GET api/profiles/test
+// @desc  Test profiles Route
+// @access  Public
+
 router.get("/test", (req, res) => res.json({ msg: "Profile works" }));
 
 module.exports = router;
