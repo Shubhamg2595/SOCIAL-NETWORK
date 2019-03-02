86d7695766c7be22006139e3c894e119b4f94b21
Shubham Gupta <shubhamg2595@gmail.com>
2019-03-02 16:44:35 +0530

    User Registration , bcrypt , schema modeling

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,54 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypyt = require("bcryptjs");
+
+//load User model
+const User = require("../../models/User");
 
 //@route  GET api/users/test
 // @desc  Test users Route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Users works" }));
+
+//@route  GET api/users/register
+// @desc  Register a User :
+// User.findOne is  being used  to find whether
+// an email already exist  in db , which will be
+// provided by user using react Form
+
+// @access  Public
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already Exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypyt.genSalt(10, (err, salt) => {
+        bcrypyt.hash(newUser.password, salt, (err, hash) => {
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const bodyParser = require("body-parser");
+/*to get access to request body we use bodyparser*/
 const users = require("./routes/api/users");
 const profiles = require("./routes/api/profiles");
 const posts = require("./routes/api/posts");
 
 const app = express();
 
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //DB config
 const db = require("./config/keys").mongoURI;
 
 //connect to mongoDB through mongoose
 mongoose
-  .connect(db)
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MOngodb connected"))
   .catch(err => console.log(err));
 
