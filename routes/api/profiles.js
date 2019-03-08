f7bcba71f70f58b2db1b39c6ead7578b582c678b
Shubham Gupta <shubhamg2595@gmail.com>
2019-03-08 18:52:45 +0530

    Route for Current User Profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -17,6 +17,21 @@ router.get("/test", (req, res) => res.json({ msg: "Profile works" }));
 //@route  GET api/profiles
 // @desc  Get current users Profile
 // @access  Private as user  willl only be able to see the profile if user has successfully logged in
-router.get("/", passport.authenticate("jwt", { session: false }), () => {});
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors.noprofile);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
