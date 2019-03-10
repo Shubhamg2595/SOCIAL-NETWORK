b86ec61a63e0d6fea49a5cd787a88e1d7a6e997c
Shubham Gupta <shubhamg2595@gmail.com>
2019-03-10 15:22:16 +0530

    Registration and Error Reducer ,withRouter,mapStateToProps

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,8 +1,18 @@
 //Register User
-import { TEST_DISPATCH } from "./types";
-export const registerUser = userData => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  };
+import axios from "axios";
+import { GET_ERRORS } from "./types";
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post("/api/users/register", userData)
+    .then(result => history.push("login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 };
+// axios
+//   .post("/api/users/register", newUser)
+//   .then(result => console.log(result.data))
+//   .catch(err => this.setState({ errors: err.response.data }));
